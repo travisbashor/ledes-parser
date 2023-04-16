4b0c8a94395d64eca426cfb003e21c5b56bfda6a
Travis Bashor <travis.bashor@gmail.com>
2023-04-15 23:17:56 -0400

    fix(parser): skip header, spec description during parsing

--- ledes_parser/ledes_1998_parser.py
+++ ledes_parser/ledes_1998_parser.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List, TextIO
 
 from ledes_parser.base_ledes_parser import BaseLedesParser
@@ -8,38 +9,19 @@ from .typings.invoice_types import Invoice
 class Ledes1998Parser(BaseLedesParser):
     def parse(self, reader: TextIO) -> List[Invoice]:
         invoices = []
+
+        # Skip the format spec and header lines.
+        print(next(reader))
+        print(next(reader))
         for row in reader:
             # Parse fields according to the 1998 format.
             if not row:
                 continue
 
-            record_type = row[0]
+            current_invoice = Invoice()
+            date_str = row[0]
+            current_invoice['invoice_date'] = datetime.strptime(date_str, "%Y%m%d")
 
-            if record_type == 'INVOICE':
-                invoice = {
-                    'invoice_number': row[1],
-                    'client_id': row[2],
-                    'invoice_date': row[3],
-                    'invoice_total': row[4],
-                    'bill_to': row[5],
-                    'invoice_description': row[6],
-                    'line_item_count': row[7],
-                    'invoice_lines': []
-                }
-                invoices.append(invoice)
-            elif record_type == 'LINE':
-                invoice_line = {
-                    'line_number': row[1],
-                    'expense_date': row[2],
-                    'timekeeper_id': row[3],
-                    'timekeeper_name': row[4],
-                    'hours': row[5],
-                    'rate': row[6],
-                    'task_code': row[7],
-                    'activity_code': row[8],
-                    'description': row[9],
-                    'line_total': row[10]
-                }
-                invoices[-1]['invoice_lines'].append(invoice_line)
+            invoices.append(current_invoice)
 
         return invoices
