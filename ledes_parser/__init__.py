75e8a03517612410322bb33fdca1f3d0aec19b0c
Travis Bashor <travis.bashor@gmail.com>
2023-04-15 22:19:23 -0400

    fix: update import path, fix bug with misnamed format variable in parse_ledes_file

--- ledes_parser/__init__.py
+++ ledes_parser/__init__.py
@@ -1,22 +1,25 @@
 import csv
 from typing import List, TextIO
-from ledes_1998_parser import Ledes1998Parser
-from ledes_1998B_parser import Ledes1998BParser
-from ledes_1998BI_parser import Ledes1998BIParser
+
+from ledes_parser.ledes_1998_parser import Ledes1998Parser
+from ledes_parser.ledes_1998B_parser import Ledes1998BParser
+from ledes_parser.ledes_1998BI_parser import Ledes1998BIParser
+
 from .typings.invoice_types import Invoice
+
 
 def parse_ledes_file(file_path: TextIO, ledes_format: str) -> List[Invoice]:
     with open(file_path, newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
 
         parser = None
-        if format == '1998':
+        if ledes_format == '1998':
             parser = Ledes1998Parser()
-        elif format == '1998B':
+        elif ledes_format == '1998B':
             parser = Ledes1998BParser()
-        elif format == '1998BI':
+        elif ledes_format == '1998BI':
             parser = Ledes1998BIParser()
         else:
-            raise ValueError(f'Unsupported format: {format}')
+            raise ValueError(f'Unsupported format: {ledes_format}')
 
         return parser.parse(reader)
