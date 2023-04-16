646f3f8c07caebf50a7094a871a3daf00fea9603
Travis Bashor <travis.bashor@gmail.com>
2023-04-15 20:05:52 -0400

    feat: add initial files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ledes-parser',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    author='Travis Bashor',
+    author_email='travis.bashor@gmail.com',
+    description='A package for parsing LEDES format files',
+    url='https://github.com/travisbashor/ledes-parser',
+)
