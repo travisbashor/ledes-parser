eaaecd44368bc96828f8b8c2670768a6b0380edc
Travis Bashor <travis.bashor@gmail.com>
2023-04-21 13:26:19 -0400

    ci: auto-bump version number on release

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup, find_packages
 
-def read_version():
-    return '1.0.1'
 
-setup(
-    name='ledes-parser',
-    version=read_version(),
-    packages=find_packages(),
-    install_requires=[
-        # List your package dependencies here
-    ],
-    author='Travis Bashor',
-    author_email='travis.bashor@gmail.com',
-    description='A package for parsing LEDES format files',
-    url='https://github.com/travisbashor/ledes-parser',
-)
+def main() -> None:
+    import ledes_parser as app
+
+    setup(
+        name='ledes-parser',
+        version=app.__version__,
+        packages=find_packages(),
+        install_requires=[
+            # List your package dependencies here
+        ],
+        author='Travis Bashor',
+        author_email='travis.bashor@gmail.com',
+        description='A package for parsing LEDES format files',
+        url='https://github.com/travisbashor/ledes-parser',
+    )
+
+
+if __name__ == "__main__":
+    main()
