410d8b8cbedf45c453107729ae727664dfded596
Travis Bashor <travis.bashor@gmail.com>
2024-06-08 11:06:44 -0400

    chore: throw out experimental files

--- tests/test_parse_leads_file.py
+++
@@ -1,8 +0,0 @@
-from ledes_parser import parse_ledes_file
-
-
-def test_parse_ledes_file_with_file_path_should_succeed():
-    with open("tests/ledes_98B_sample_file.txt") as f:
-        invoices = parse_ledes_file(f, "1998B")
-
-    assert len(invoices) > 0
