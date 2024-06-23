d7f1c2823157f59baffbbae92fe6d64a5f922fee
Travis Bashor <travis.bashor@gmail.com>
2024-06-23 11:39:05 -0400

    feat: validate spec

--- ledes_parser/ledes_parser.py
+++ ledes_parser/ledes_parser.py
@@ -7,6 +7,11 @@ def get_parser(spec: str) -> Lark:
     """
     @returns A parser that can read ledes text in the given ledes spec (e.g., 98B, 98BI, etc...).
     """
+    supported_specs = ["98B"]
+    if spec not in supported_specs:
+        raise ValueError(
+            f"Unrecognized ledes spec: '{spec}'. Supported specs are: {supported_specs}"
+        )
     # By convention, the grammar for a spec is in: grammars/spec_<the-spec>/main_grammar.lark
     folder_name = f"grammars/spec_{spec.upper()}/main_grammar.lark"
     path_to_grammar = pkg_resources.resource_filename(__name__, folder_name)
