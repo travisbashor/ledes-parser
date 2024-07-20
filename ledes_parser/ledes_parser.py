56a8ce77533d902319e2000693d5f5a58216bee3
Travis Bashor <travis.bashor@gmail.com>
2024-07-20 17:18:18 -0400

    style: remove quotes from return type of get_parser()

--- ledes_parser/ledes_parser.py
+++ ledes_parser/ledes_parser.py
@@ -24,7 +24,7 @@ class UnsupportedLEDESVersionError(NotImplementedError):
     pass
 
 
-def get_parser(spec: SupportedSpecs, ast_only: bool = False) -> "Lark":
+def get_parser(spec: SupportedSpecs, ast_only: bool = False) -> Lark:
     """
     @returns A parser that can read ledes text in the given ledes spec (e.g., 1998B, 1998BI, etc...).
     @param ast_only: Whether the parser should return just the abstract syntax tree, instead of mapping the tokens to a dict.
